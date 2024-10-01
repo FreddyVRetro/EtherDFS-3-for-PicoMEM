9c2e554ce9ff83643c3171ff519a13c8afa0299e
FreddyV <freddy.vetele@gmail.com>
2024-10-01 16:50:53 +0200

    update BIOS command

--- pm_s_lib.h
+++ pm_s_lib.h
@@ -1,6 +1,7 @@
 #pragma once
 // Basic PicoMEM library full include, to use with any C Code
 
+#define PM_ETHDFS 1
 #define TEST 1   // 1 for Test Mode (No PicoMEM)
 
 // * Status and Commands definition
@@ -13,10 +14,15 @@
 
 #define DEFAULT_BASE 0x2A0
 
-/* For ETHDFS : Need to declare in the data segment
-unsigned short PM_Base=0;
-unsigned short BIOS_Segment=0;
-*/
+#if (PM_ETHDFS==0)
+// For ETHDFS : Need to declare in the data segment
+unsigned short PM_Base=0;         // PicoMEM I/O Base address
+unsigned char  PM_PicoID=0;       // Pi Pico board model/ID 
+unsigned char  PM_BoardID=0;      // PicoMEM Board model/ID
+unsigned short PM_FW_Rev=0;       // PicoMEM firmware Revision
+unsigned short BIOS_Segment=0;    // PicoMEM BIOS segment (Can be 0 if not detected)
+unsigned short PM_PCCR_Param=0;   // Commands parameter RAM address (To send/Receive small data to/from command)
+#endif
 
 
 bool pm_wait_cmd_end()
@@ -77,6 +83,58 @@ unsigned short pm_io_cmd(unsigned char cmd,unsigned short arg)
 ;         DX : AA55h (Means Ok)
 */
 extern bool pm_irq_detect()
+{
+#if TEST    // Return fake PicoMEM Status
+ BIOS_Segment=0xD000;
+ PM_Base=0x220;
+ return true;
+#else
+bool r;
+_asm {
+mov ax,0x6000
+mov dx,0x1234
+int 0x13
+mov PM_Base,ax
+#if (PM_ETHDFS==0)
+inc ax
+mov PM_DataL,ax
+inc ax
+mov PM_DataH,ax
+#endif
+mov BIOS_Segment,bx
+/* mov PM_DeviceMask,cx */
+cmp dx,0xAA55
+jne @@no_bios
+/*PM BIOS Function 3 : Return the PicoMEM Board ID and BIOS RAM Offset
+; Added in Sept 2024
+; Return AH : Pi Pico Board / chip ID
+;        AL : PicoMEM Board ID
+;        BX : Firmware Revision
+;        CX : PCCR_Param (Offset of commands response)
+;        DX : Reserved for the future */
+mov ax,0x6003
+mov dx,0x1234
+mov cx,0xFFFF         //For BIOS fonction detect
+int 0x13
+// If CX is still 0xFFFF the BIOS Does not support this fonction.
+cmp cx,0FFFF
+je @no_bios_3
+mov PM_BoardID,al     // Collect the different infos
+mov PM_PicoID,ah
+mov PM_PCCR_Param,cx
+@no_bios_3:           // All the linked variables remains at 0
+mov al,1              // Return true
+@@no_bios:
+mov al,0              // Return false
+@@end:
+mov r,al
+};
+return r;
+#endif
+}
+
+
+extern bool pm_get_id()
 {
 #if TEST    // Return fake PicoMEM Status
  BIOS_Segment=0xD000;
